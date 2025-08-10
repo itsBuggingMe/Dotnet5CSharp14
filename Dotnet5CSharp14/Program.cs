a1b01ff83928814544db54f94e6f74a0f70847ba
itsBuggingMe <lohj002@gmail.com>
2025-08-10 10:18:47 -0400

    Add project files.

---
+++ Dotnet5CSharp14/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Dotnet5CSharp14;
+
+internal partial class CSharp13
+{
+    static void Main(string[] args) { }
+    public void SpanParams(params ReadOnlySpan<int> ints) { }
+    public void IEnumerableParams(params IEnumerable<int> ints) { }
+    public partial string PartialProp { get; }
+    public partial string PartialProp => "Impl";
+}
+
+
+internal class CSharp12(int x)
+{
+    public int PrimaryCtor = x;
+    public int[] CollectionExpr = [1, 2, 3];
+    public void MoreCollExpr()
+    {
+        Span<int> ints = [1, 2, 3];
+        IEnumerable<int> ints2 = [1, 2, 3];
+        int[] moreInts = [.. ints, .. ints2];
+    }
+    public void RefReadonly(ref readonly int x) { }
+}
+
+file class CSharp11
+{
+    [Generic<string>]
+    public class GenericAttribute<T> : Attribute { }
+    nint Nint;
+    nuint Nuint;
+    public string RawStringLiteral => """
+        Hello, World
+        Newlines here.
+        """;
+
+    public static void ScopedRefAndLocalVariables(scoped ref int x)
+    {
+        scoped Span<int> s = stackalloc int[10];
+    }
+}
+
+internal class CSharp10
+{
+    public void NaturalLambdaType()
+    {
+        var fun = string.IsNullOrEmpty;
+    }
+
+    public Func<int> AttributesOnLambdas(Func<int> func) => AttributesOnLambdas([MethodImpl] () => 0);
+
+    public record struct A;
+    public readonly record struct B;
+    public record class C(int X);
+
+    public C WithKeyword(C c) => c with { X = 1 };
+
+    public const string ConstInterpolatedString = $"This is {nameof(CSharp10)}";
+}
